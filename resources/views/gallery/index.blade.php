584aa49a6ebc299a2cc531405c967f81d125f2e4
naomi-rc <naomi.cat182128@gmail.com>
2019-01-08 16:02:58 -0500

    commit to save

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -1,1 +1,42 @@
-<p>gallery</p>
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+
+
+	<title>Gallery</title>
+
+<style>
+	body {
+		/*background-image: url('https://static.gamespot.com/uploads/screen_medium/1179/11799911/3471748-screen+shot+2018-12-06+at+3.20.53+pm.png');*/
+		background-repeat: no-repeat;
+	}
+
+
+	
+</style>
+</head>
+<body>
+	<div>
+		<img src="{{asset('default.png')}}" alt="huh?"/>
+		<?php
+
+			$files = glob('../resources/images/gallery/*.{jpg,png,gif}', GLOB_BRACE);
+		//$files = scandir('../resources/images/gallery');
+			
+			foreach($files as $file) {
+			 print("<img src='default.png' />");
+			}
+		?>
+	</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
