1dc87ca209cc0cadfd3a7eb521e7accacc70eb67
naomi-rc <naomi.cat182128@gmail.com>
2019-01-10 13:06:35 -0500

    fixed images scroll

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -5,12 +5,17 @@
 <?php 
 	$arr = ["hi", "there", "amiga"]; 
 	$files = glob('./images/gallery/*.{jpg,png,gif}', GLOB_BRACE);
-	
+	// @foreach($files as $file)
+	//<img src='{{$file}}'/>
+	//@endforeach
 ?>
-
-  @foreach($files as $file)
-  	<img src='{{$file}}'/>
-  @endforeach
+	<div class="scrollable">
+	 @for($i = 0; $i < 100; $i++)
+	 	
+	  		<img src='./images/gallery/default.png' style="width: 110px; height: 110px; border: 2px solid grey; margin: 3px;"/>
+	 
+	  @endfor
+	</div>
 @endsection
 
 <style>
@@ -19,4 +24,11 @@
 		background-repeat: no-repeat;
 		background-size: 100% 200%; 
 	}
+
+	.scrollable {
+        float:left;
+        height: 80%;
+		overflow-y: scroll;		
+		padding: 20px;
+     }
 </style>
