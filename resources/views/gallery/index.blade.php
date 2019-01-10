a9722f9f3cc27fb4f84ff5510f12f2ef5340a834
naomi-rc <naomi.cat182128@gmail.com>
2019-01-10 12:45:37 -0500

    added background image

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -2,23 +2,21 @@
 
 @section('content')
 
-   <div>
-		<img src='/../resources/images/gallery/default.png' alt="huh?"/>
-		<?php
-		//$path = "{{asset('/default.png')}}";
-		 
-		$files = glob('../resources/images/gallery/*.{jpg,png,gif}', GLOB_BRACE);
-		//$files = scandir('../resources/images/gallery');
-		//var_dump($files);
-			//var_dump($files[2]);
-			//foreach($files as $file) {
-			 	print("<img src='/../resources/default.png' />");
+<?php 
+	$arr = ["hi", "there", "amiga"]; 
+	$files = glob('./images/gallery/*.{jpg,png,gif}', GLOB_BRACE);
+	
+?>
 
-			//}
-
-
-
-
-		?>
-	</div>
+  @foreach($files as $file)
+  	<img src='{{$file}}'/>
+  @endforeach
 @endsection
+
+<style>
+	body {
+		background-image: url('./images/mib_bg.png');
+		background-repeat: no-repeat;
+		background-size: 100% 200%; 
+	}
+</style>
