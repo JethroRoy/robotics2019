41bd73383e669506ab4aa2acb55f220fd4ab18b3
naomi-rc <naomi.cat182128@gmail.com>
2019-02-02 15:58:43 -0500

    save for gallery

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -4,7 +4,6 @@
 <link href="{{asset('css/gallery_styles.css')}}" rel="stylesheet" type="text/css"/>
 <?php 
 	$files = glob('img/gallery/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
-	//src="{{ asset('img/gear.png') }}"
 ?>
 	<div class="gallery scrollable">
 	 @foreach($files as $file)	 	
@@ -17,12 +16,6 @@
 	</div>
 @endsection
 
-<style>
- 	
-	     
-
-
-</style>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script type="text/javascript">
 	$(function()
