0e03350e4eea354a23b6f399c4d87653b4e1c116
Jethro Roy <jethroroy@gmail.com>
2019-02-02 17:12:00 -0500

    Finish Game page in english

--- resources/views/game/index.blade.php
+++ resources/views/game/index.blade.php
@@ -1,8 +1,45 @@
 
 @extends ('layouts.master')
 
-@section('content')
+@section('body')
+    <div class="game1"></div>
 
-    <p>game</p>
+    <div class="background-gradient">
+        <h1 class="text-title"> About Vertigoal </h1>
+        Following the traditional structure of the annual secondary and cegep level Canadian Robotics Competition, the game will be played in teams; blue and yellow. Each team will be composed of two different schools’ robots competing alongside each other against the robots of two other schools. Points earned by the school is equal to the shared points of the colour they were on, assuming they did not acquire any individual penalties. <br/> <br/>
+        Each school is allowed to have two humans seated around the playing field and one robot on the playing field. One human is the designated pilot who will have the VEX controller and operate the robot, while the other acts as a copilot who will assist the pilot by providing an extra set of eyes on the field. Neither the pilot nor the copilot are allowed to interact directly with the robot or the playing field. <br/> <br/>
+        During the preliminaries, the teams will follow a round-robin rotation of five minute heats, to try to allow for every school to have the opportunity to play either with or against every other school. The scores accumulated during the preliminary round-robin, which takes up the Friday of the competition, by each individual school will determine their placement. The top teams, along with the teams who make up their scores in the knockout rounds early Saturday morning, move on to the quarter-finals. The top 8 teams compete in the semi-finals, of which the top four teams complete one last round-robin amongst themselves in the finals to determine who will be victorious.
+    </div>
+
+    <div class="game2"></div>
+
+    <div class="background-gradient">
+        <h1 class="text-title"> The Goal </h1>
+        Vertigoal 2019 has, as its primary goal, to make as many points as possible in teams of two during each five minute heat. On the field, there is a total of 142 standard tennis balls; 70 “yellow”, 70 “blue” and 2 “red”. Each team is responsible for making points by putting the balls of their team’s colour in the targets. Each robot is allowed to begin the heat with a maximum of 10 balls of its team’s colour already loaded. The winning team is the team that has scored the most points, depending on the amount of their own colour in each target and taking multipliers into account. <br/> <br/>
+        The field is separated into five different levels, each at a different elevation: level 1 (left of image) is the lowest and level 5 (right of image) is the highest. The robots are allowed to begin the heat in one of three starting points for their team colour in levels 1, 3, and 5. <br/> <br/>
+        There are three types of targets present on the field, each with different points attributed: 10, 20 or 40 depending on the container height. <br/> <br/>
+        At the end of each heat, the points are calculated by counting the amount of yellow game pieces in each target multiplied by the points attributed to each target for the yellow team’s score and the same process using blue game pieces for the blue team’s score, regardless of which team is responsible for putting the game piece in the target.
+    </div>
+
+    <div class="game3"></div>
+
+    <div class="background-gradient">
+        <h1 class="text-title"> The Multiplier </h1>
+        If a multiplier (red ball) is present in a target, it affects the points for all game pieces in that target, both blue and yellow, regardless of which team placed it. Only one multiplier will be counted per bucket, therefore if both red balls are in the bucket, the total points for that bucket are doubled for both the yellow and blue balls present. The red balls alone are not worth any points. <br/> <br/>
+        The points awarded to individual schools equal the total points scored by their team. The only exception is if a school’s robot acquired an individual penalty during the heat or did not contribute any points to the heat.
+    </div>
+
+    <div class="game4"></div>
+
+    <div class="background-gradient">
+        <h1 class="text-title"> Penalties </h1>
+        Just as points are awarded for excelling in the game and playing by the rules, points are also deducted when a team infringes these rules. The penalties for the game fall into specific categories: safety, respect for the space and sportsmanlike conduct. All penalties are given at the discretion of the referees and judges.
+    </div>
+
+
+
+@endsection
+
+@section('content')
 
 @endsection
--- resources/views/robot/index.blade.php
+++ resources/views/robot/index.blade.php
@@ -1,8 +1,18 @@
 
 @extends ('layouts.master')
 
-@section('content')
+@section('body')
+    <div class="vanier1"></div>
 
-    <p>robot</p>
+    <div class="background-gradient">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce suscipit est at dolor porttitor tincidunt. Duis at est quis arcu vestibulum venenatis. In interdum, ipsum sodales sollicitudin condimentum, leo massa mollis justo, sit amet tristique orci lorem ac libero. Pellentesque non pellentesque felis. Etiam fringilla vel sem eget rhoncus. Maecenas ut massa ex. Pellentesque aliquam vehicula dui, in mattis lacus maximus in. Etiam quis nulla pellentesque, volutpat orci vel, tristique leo. Cras eu suscipit lacus. In mattis nisi nec viverra placerat. Morbi ullamcorper commodo sapien. Mauris sem diam, fringilla in tellus accumsan, commodo faucibus nisi. Fusce mauris nulla, venenatis eget quam at, blandit placerat velit. Maecenas vulputate lacus a turpis consequat, vel aliquet erat dapibus. Duis elementum ornare blandit.
+    </div>
+
+    <div class="vanier2"></div>
+
+    <div class="background-gradient">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce suscipit est at dolor porttitor tincidunt. Duis at est quis arcu vestibulum venenatis. In interdum, ipsum sodales sollicitudin condimentum, leo massa mollis justo, sit amet tristique orci lorem ac libero. Pellentesque non pellentesque felis. Etiam fringilla vel sem eget rhoncus. Maecenas ut massa ex. Pellentesque aliquam vehicula dui, in mattis lacus maximus in. Etiam quis nulla pellentesque, volutpat orci vel, tristique leo. Cras eu suscipit lacus. In mattis nisi nec viverra placerat. Morbi ullamcorper commodo sapien. Mauris sem diam, fringilla in tellus accumsan, commodo faucibus nisi. Fusce mauris nulla, venenatis eget quam at, blandit placerat velit. Maecenas vulputate lacus a turpis consequat, vel aliquet erat dapibus. Duis elementum ornare blandit.
+    </div>
+@endsection
+
+@section('content')
 
 @endsection
