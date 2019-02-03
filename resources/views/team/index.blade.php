ec3964b018427cd1c08dc215b26295dfc6716809
naomi-rc <naomi.cat182128@gmail.com>
2019-02-03 17:57:27 -0500

    revert to original

--- resources/views/team/index.blade.php
+++ resources/views/team/index.blade.php
@@ -34,7 +34,7 @@
             $files = glob('img/teams/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
         ?>
         @foreach($files as $file) 
-            <div class="member">
+            <div class="image member">
                 <img class="member-image" src="{{ asset($file) }}" alt="member">
                
             </div>
@@ -45,10 +45,30 @@
     </div>
 </div><!-- /.container -->
 <div class="panel" >
-
+    <img id="panel-image" src="{{ asset('img/teams/test.png') }}"/>
 </div>
 
 @include('layouts.footer')
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+    $(function()
+    {
+        $('.image').click(function(){
+            var path = $(this).attr('src');
+            $('#panel-image').attr('src', path);
+
+            /*var modal = $('.viewer');
+            modal.show();
+
+            $('.close').click(function(){
+                modal.hide();
+            });*/
+        });
+    });
+</script>
+
 
 </body>
 </html>
