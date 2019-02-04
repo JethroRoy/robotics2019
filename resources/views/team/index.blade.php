8a2baf31d6fb8b7148320a2f47ba8627feffe42d
naomi-rc <naomi.cat182128@gmail.com>
2019-02-04 00:01:53 -0500

    commit to save teams

--- resources/views/team/index.blade.php
+++ resources/views/team/index.blade.php
@@ -3,25 +3,76 @@
 @section('content')
 <link href="{{asset('css/team_styles.css')}}" rel="stylesheet" type="text/css"/>
 <?php 
-    $files = glob('img/teams/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+    $buildCaptains = glob('img/teams/build/captain/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+    $build = glob('img/teams/build/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+
+    $kioskCaptains = glob('img/teams/kiosk/captain/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+    $kiosk = glob('img/teams/kiosk/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+
+    $journalismCaptains = glob('img/teams/journalism/captain/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+    $journalism = glob('img/teams/journalism/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+
+    $webCaptains = glob('img/teams/web/captain/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+    $web = glob('img/teams/web/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+
+    $videoCaptains = glob('img/teams/video/captain/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+    $video = glob('img/teams/video/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
 ?>
 <div class="fill" >
 
     <div class="blue-ring neon-blue">
-        
-        @foreach($files as $file) 
-           
-                <img class="image" src="{{ asset($file) }}" alt="member"/>
-               
-            
+        @foreach($buildCaptains as $file)           
+            <img class="image" src="{{ asset($file) }}" alt="member"/>       
         @endforeach
+         @foreach($build as $file)           
+            <img class="image" src="{{ asset($file) }}" alt="member"/>       
+        @endforeach
+    </div>
 
+    <div class="blue-ring neon-blue" hidden> 
+        @foreach($kioskCaptains as $file) 
+            <img class="image" src="{{ asset($file) }}" alt="member"/>            
+        @endforeach      
+        @foreach($kiosk as $file) 
+            <img class="image" src="{{ asset($file) }}" alt="member"/>            
+        @endforeach  
+    </div>
 
+    <div class="blue-ring neon-blue" hidden>
+        @foreach($journalismCaptains as $file) 
+            <img class="image" src="{{ asset($file) }}" alt="member"/>
+        @endforeach
+        @foreach($journalism as $file) 
+            <img class="image" src="{{ asset($file) }}" alt="member"/>
+        @endforeach      
+    </div>
+
+    <div class="blue-ring neon-blue" hidden>
+        @foreach($webCaptains as $file) 
+            <img class="image" src="{{ asset($file) }}" alt="member"/>            
+        @endforeach
+        @foreach($web as $file) 
+            <img class="image" src="{{ asset($file) }}" alt="member"/>            
+        @endforeach
       
     </div>
+    <div class="blue-ring neon-blue" hidden>
+        @foreach($videoCaptains as $file)  
+            <img class="image" src="{{ asset($file) }}" alt="member"/>
+        @endforeach
+        @foreach($video as $file)  
+            <img class="image" src="{{ asset($file) }}" alt="member"/>
+        @endforeach
+    </div>
 </div><!-- /.container -->
+
+
 <div class="panel" >
     <img id="panel-image" src="{{ asset('img/teams/test.png') }}"/>
+
+    <div id="bio">
+        Hello
+    </div>
 </div>
 @endsection
 
@@ -34,12 +85,16 @@
             var path = $(this).attr('src');
             $('#panel-image').attr('src', path);
 
-            /*var modal = $('.viewer');
-            modal.show();
+          /*  if ($bioFile = fopen("{{ asset('bios/Houara,\ Hummam.txt') }}", "r")) {
+                while (!feof($bioFile)) {
+                    $line = fgets($bioFile);
+                    echo $line;
+                }
+                fclose($bioFile);
+            }*/
 
-            $('.close').click(function(){
-                modal.hide();
-            });*/
         });
+        $('.image').first().click();
+
     });
 </script>
